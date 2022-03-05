6ca62395491771931970da9fa81d37dca26bad8f
pyarosh <peteryarosh@gmail.com>
2022-03-05 12:20:54 -0500

    add functions for creating square, circle and cross elements

--- src/main/scala/nicoburniske/faxion/image/morph/package.scala
+++ src/main/scala/nicoburniske/faxion/image/morph/package.scala
@@ -1,13 +1,26 @@
 package nicoburniske.faxion.image
 
 package object morph {
-  val DIMENSION = 2
+  val DIMENSION                                    = 2
+  val DEFAULT_STRUCTURING_ELEMENT: Set[(Int, Int)] = circleElem(DIMENSION)
 
-  val DEFAULT_STRUCTURING_ELEMENT: Set[(Int, Int)] = {
-    val range = -DIMENSION to DIMENSION
-    for {
+  def squareElem(distanceFromCenterToEdge: Int): Set[(Int, Int)] = {
+    val range = -distanceFromCenterToEdge to distanceFromCenterToEdge
+    val res   = for {
       x <- range
       y <- range
     } yield (x, y)
-  }.filter { case (x, y) => Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2)) <= DIMENSION - 1 }.toSet
+    res.toSet
+  }
+
+  def circleElem(radius: Int): Set[(Int, Int)] = {
+    squareElem(radius).filter {
+      case (x, y) =>
+        Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2)) <= radius - 1
+    }
+  }
+
+  def crossElement(distanceFromCenterToEdge: Int): Set[(Int, Int)] = {
+    squareElem(distanceFromCenterToEdge).filter { case (x, y) => x == 0 || y == 0 }
+  }
 }
